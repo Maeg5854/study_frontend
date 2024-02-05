710d46fd2af9afeb2d102d0e46ed9d79e042849b
Maeg5854 <kimhj585407@gmail.com>
2024-02-05 23:23:13 +0900

    [nomad] ~6.1 useEffect

--- react/2_basic/react-for-mandy/src/App.js
+++ react/2_basic/react-for-mandy/src/App.js
@@ -1,10 +1,21 @@
-import Button from "./Button";
 import styles from "./App.module.css";
+import { useEffect, useState } from "react";
+
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const onClick = () => setCounter((curr) => curr + 1);
+
+  console.log("i run all the time"); // App의 상태가 바뀔때마다 실행된다
+
+  useEffect(() => {
+    console.log("Call the API");
+  }, []); // App 으ㅣ 상태가 얼마나 바뀌던지 상관없이 1번만 실행된다.
+
   return (
     <div>
-      <h1 styles={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"} />
+      <h1 styles={styles.title}>Welcome back! {counter}</h1>
+      <button onClick={onClick}>hello</button>
     </div>
   );
 }
