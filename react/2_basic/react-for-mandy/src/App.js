5b621f9512ade2dc21298be344d55d29110ed999
Maeg5854 <kimhj585407@gmail.com>
2024-02-06 01:38:55 +0900

    [nomad] ~6.3

--- react/2_basic/react-for-mandy/src/App.js
+++ react/2_basic/react-for-mandy/src/App.js
@@ -3,17 +3,33 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
 
   const onClick = () => setCounter((curr) => curr + 1);
-
-  console.log("i run all the time"); // App의 상태가 바뀔때마다 실행된다
+  const onChange = (event) => setKeyword(event.target.value);
 
   useEffect(() => {
-    console.log("Call the API");
+    console.log("I run only once");
   }, []); // App 으ㅣ 상태가 얼마나 바뀌던지 상관없이 1번만 실행된다.
+
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when keyword & counter change");
+  }, [keyword, counter]);
 
   return (
     <div>
+      <input
+        type="text"
+        placeholder="search here..."
+        value={keyword}
+        onChange={onChange}
+      />
       <h1 styles={styles.title}>Welcome back! {counter}</h1>
       <button onClick={onClick}>hello</button>
     </div>
