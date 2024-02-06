c810ce37665e11ac1e2a90943949764381c4398f
Maeg5854 <kimhj585407@gmail.com>
2024-02-06 23:00:41 +0900

    [nomad] ~6.4 effect clean up

--- react/2_basic/react-for-mandy/src/App.js
+++ react/2_basic/react-for-mandy/src/App.js
@@ -1,37 +1,20 @@
 import styles from "./App.module.css";
 import { useEffect, useState } from "react";
 
+function Hello() {
+  return <h1>Hello</h1>;
+}
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState("");
-
-  const onClick = () => setCounter((curr) => curr + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   useEffect(() => {
-    console.log("I run only once");
-  }, []); // App 으ㅣ 상태가 얼마나 바뀌던지 상관없이 1번만 실행된다.
-
-  useEffect(() => {
-    console.log("I run when 'keyword' changes.");
-  }, [keyword]);
-  useEffect(() => {
-    console.log("I run when 'counter' changes.");
-  }, [counter]);
-  useEffect(() => {
-    console.log("I run when keyword & counter change");
-  }, [keyword, counter]);
-
+    console.log("showing!!");
+    return () => console.log("hide!!");
+  }, [showing]);
   return (
     <div>
-      <input
-        type="text"
-        placeholder="search here..."
-        value={keyword}
-        onChange={onChange}
-      />
-      <h1 styles={styles.title}>Welcome back! {counter}</h1>
-      <button onClick={onClick}>hello</button>
+      <button onClick={onClick}>{showing ? "hide" : "show"}</button>
+      {showing ? <Hello /> : null}
     </div>
   );
 }
