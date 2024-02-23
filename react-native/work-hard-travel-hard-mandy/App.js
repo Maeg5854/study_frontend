a4920594fa78803d233f873566c9c429ee0fae11
Maeg5854 <kimhj585407@gmail.com>
2024-02-23 23:55:12 +0900

    [nomad] 3.1 touchable

--- react-native/work-hard-travel-hard-mandy/App.js
+++ react-native/work-hard-travel-hard-mandy/App.js
@@ -1,11 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  TouchableHighlight,
+  TouchableWithoutFeedback,
+  View,
+  Pressable,
+} from "react-native";
+import { theme } from "./colors";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity>
+          <Text style={styles.btnText}>Touchable</Text>
+        </TouchableOpacity>
+        <TouchableHighlight onPress={() => console.log("pressed TH")}>
+          <Text style={styles.btnText}>TouchableHighlight</Text>
+        </TouchableHighlight>
+        <TouchableWithoutFeedback onPress={() => console.log("pressed TWF")}>
+          <Text style={styles.btnText}>TouchableWithoutFeedback</Text>
+        </TouchableWithoutFeedback>
+        <Pressable
+          onPress={() => console.log("pressed Pr")}
+          TouchableOpacity={0.5}
+        >
+          <Text style={styles.btnText}>Pressable</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
@@ -13,8 +38,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.bg,
+    paddingHorizontal: 20,
+  },
+  header: {
+    flexDirection: "column",
+    marginTop: 100,
+    justifyContent: "space-between",
+  },
+  btnText: {
+    color: "white",
+    fontSize: 38,
+    fontWeight: "600",
   },
 });
